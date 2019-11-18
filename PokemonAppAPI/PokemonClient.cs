a829bd3d99124c7f0a7be28f5ea90144fd464fb0
ness6514 <ness6514@student.cptc.edu>
2019-11-18 14:36:26 -0800

    Add project files.

---
+++ PokemonAppAPI/PokemonClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PokemonAppAPI
+{
+    public class PokemonClient
+    {
+        private static readonly HttpClient _client;
+
+        static PokemonClient()
+        {
+            _client = new HttpClient();
+        }
+
+        /// <summary>
+        /// Returns a single Pokemons data using Id
+        /// </summary>
+        /// <returns></returns>
+        public async Task<PokemonResponse> GetPokemon(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns a single Pokemons data using name
+        /// </summary>
+        /// <returns></returns>
+        public async Task<PokemonResponse> GetPokemon(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
